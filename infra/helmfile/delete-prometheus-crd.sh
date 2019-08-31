b9407a6d7196047cff7d043b6db92e6a9e8b152f
Manikandan Natarajan <manikandan.natarajan@anz.com>
2019-08-31 15:30:02 +0800

    Upgrade cadence, cassandra, prometheus and grafana docker images

--- infra/helmfile/delete-prometheus-crd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash -ex
-
-kubectl get crd -o json | jq -Mr '.items[] | select(.spec.group == "monitoring.coreos.com") | .metadata.name' | while read CRD; do
-    echo "Deleting CRD $CRD"
-    kubectl delete crd $CRD
-done
