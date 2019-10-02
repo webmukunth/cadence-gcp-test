ea80da11639faad5f05c5e6a74fffeb8163845cf
Manikandan Natarajan <manikandan.natarajan@anz.com>
2019-10-02 17:47:53 +0800

    Some changes

--- commons-api/src/main/java/com/anz/cadence/commons/data/PaymentRequest.java
+++ commons-api/src/main/java/com/anz/cadence/commons/data/PaymentRequest.java
@@ -2,13 +2,9 @@ package com.anz.cadence.commons.data;
 
 import com.anz.cadence.commons.model.payment.ComAnzPmtAddRqType;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.experimental.NonFinal;
-import lombok.experimental.Wither;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
