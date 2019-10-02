f33da8881ea23bbd918c04a6e754bede61334a68
Manikandan Natarajan <manikandan.natarajan@anz.com>
2019-10-02 14:56:25 +0800

    Refactor

--- commons-api/src/main/java/com/anz/cadence/commons/data/PaymentRequest.java
+++ commons-api/src/main/java/com/anz/cadence/commons/data/PaymentRequest.java
@@ -1,23 +1,31 @@
 package com.anz.cadence.commons.data;
 
 import com.anz.cadence.commons.model.payment.ComAnzPmtAddRqType;
-import lombok.AccessLevel;
-import lombok.Data;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import lombok.experimental.Wither;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
 @Document
-@RequiredArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Value
+@Wither
+@Builder
+@NoArgsConstructor
 public class PaymentRequest {
 
   @Id
+  @NonFinal
   @NonNull
+  @Setter
   private String id;
+
+  @NonFinal
   @NonNull
+  @Setter
   private ComAnzPmtAddRqType pmtAddRqType;
 }
